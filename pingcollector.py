ef7ccd103dc15b5c0686213872fd636bb0d0ef40
Anze <info@grafolean.com>
2019-12-07 14:58:38 +0100

    On startup, wait for Grafolean backend to be available

--- pingcollector.py
+++ pingcollector.py
@@ -83,10 +83,34 @@ class PingCollector(Collector):
         send_results_to_grafolean(job_info['backend_url'], job_info['bot_token'], job_info['account_id'], values)
 
 
+def wait_for_grafolean(backend_url):
+    while True:
+        url = '{}/status/info'.format(backend_url)
+        log.info("Checking Grafolean status...")
+        try:
+            r = requests.get(url)
+            r.raise_for_status()
+            status_info = r.json()
+            if status_info['db_migration_needed'] == False and status_info['user_exists'] == True:
+                log.info("Grafolean backend is ready.")
+                return
+        except:
+            pass
+        log.info("Grafolean backend not available / initialized yet, waiting.")
+        time.sleep(10)
+
+
 if __name__ == "__main__":
     dotenv.load_dotenv()
 
     backend_url = os.environ.get('BACKEND_URL')
+    jobs_refresh_interval = int(os.environ.get('JOBS_REFRESH_INTERVAL', 120))
+
+    if not backend_url:
+        raise Exception("Please specify BACKEND_URL and BOT_TOKEN / BOT_TOKEN_FROM_FILE env vars.")
+
+    wait_for_grafolean(backend_url)
+
     bot_token = os.environ.get('BOT_TOKEN')
     if not bot_token:
         # bot token can also be specified via contents of a file:
@@ -95,9 +119,8 @@ if __name__ == "__main__":
             with open(bot_token_from_file, 'rt') as f:
                 bot_token = f.read()
 
-    if not backend_url or not bot_token:
-        raise Exception("Please specify BACKEND_URL and BOT_TOKEN / BOT_TOKEN_FROM_FILE env vars.")
-    jobs_refresh_interval = int(os.environ.get('JOBS_REFRESH_INTERVAL', 120))
+    if not bot_token:
+        raise Exception("Please specify BOT_TOKEN / BOT_TOKEN_FROM_FILE env var.")
 
     c = PingCollector(backend_url, bot_token, jobs_refresh_interval)
     c.execute()
